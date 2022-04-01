24d41bf2460aa4d24848a5333a5a25691a1be5b3
mhamadkadiry <mhamad.kadiry1@gmail.com>
2022-04-01 20:52:53 +0300

    Creating signIn and register ui

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <section>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login into your account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              id="email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              placeholder="Your password"
+              id="password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Login;
